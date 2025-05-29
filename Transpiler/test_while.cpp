4a3385078dc34baf9fed6a393efe2c3a343f9bc4
pandeydivyanka <134594216+pandeydivyanka@users.noreply.github.com>
2025-05-29 16:31:02 +0530

    Update test_while.cpp

--- Transpiler/test_while.cpp
+++ Transpiler/test_while.cpp
@@ -2,14 +2,38 @@
 using namespace std;
 
 int main() {
-    int count = 1;
+    int start, end;
+
+    // Ask user for input
+    cout << "Enter the starting number: ";
+    cin >> start;
     
-    while (count <= 5) {
-        cout << "Count is: " << count << endl;
-        count = count + 1;
+    cout << "Enter the ending number: ";
+    cin >> end;
+
+    // Check if start is less than or equal to end
+    if (start > end) {
+        cout << "Starting number must be less than or equal to the ending number." << endl;
+    } else {
+        int count = start;
+
+        // Loop from start to end
+        while (count <= end) {
+            cout << "Count is: " << count;
+
+            // Check even or odd
+            if (count % 2 == 0) {
+                cout << " (even)";
+            } else {
+                cout << " (odd)";
+            }
+
+            cout << endl;
+            count++;
+        }
+
+        cout << "Final count: " << count << endl;
     }
-    
-    cout << "Final count: " << count << endl;
-    
+
     return 0;
-} 
+}
