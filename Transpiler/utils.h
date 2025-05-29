1fec636648da1d78b584226b8df30ace90d3ffab
harshpandey91 <harshpandey1912@gmail.com>
2025-05-29 22:26:58 +0530

    Update utils.h

--- Transpiler/utils.h
+++ Transpiler/utils.h
@@ -4,7 +4,7 @@
 #include "ast.h"
 #include "symtab.h"
 
-// Function declarations
+// Function Declarations...
 DATA_TYPE str_to_type(char *type_str);
 bool search_class_body(char *var_name, struct AST_Node_Statements *root);
 void check_function_call(struct AST_Node_FunctionCall *func_call);
