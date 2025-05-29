4f2eff3d0aa13982a4b8df25489161a977e1fd76
Mohammad Arsh <143244630+arshcodesi@users.noreply.github.com>
2025-05-29 21:39:52 +0530

    Add files via upload

---
+++ test_input2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+    if (a < b) {
+        cout << "a is less than b" << endl;
+    } else {
+        cout << "a is greater than or equal to b" << endl;
+    }
+    return 0;
+}
