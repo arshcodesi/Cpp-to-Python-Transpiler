afc4f026af0c724c9d4c71008629ae9200bf45b2
Divyanshu Pant <divyanshupant30@gmail.com>
2025-05-29 21:47:00 +0530

    Add files via upload

---
+++ class_and_oblects.py
@@ -0,0 +1,10 @@
+class Name:
+    def __init__(self,Name):
+        print("new object is created")
+        self.name=Name
+
+name1=Name("divyanshu pant")
+
+print(name1.name)
+name1=Name("kartik")
+print(name1.name)
